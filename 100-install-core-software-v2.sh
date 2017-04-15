36c1f4b5272805d4d896dd40721e712a5469b1ea
Erik Dubois <erik.dubois@gmail.com>
2017-04-15 10:43:16 +0200

    Comment : updating with applications I really need on Sat Apr 15 10:43:16 CEST 2017

--- 100-install-core-software-v2.sh
+++ 100-install-core-software-v2.sh
@@ -12,21 +12,18 @@ set -e
 ##################################################################################################################
 
 #software from 'normal' repositories
-sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
+sudo pacman -S --noconfirm --needed archey3 baobab bleachbit clementine 
 sudo pacman -S --noconfirm --needed darktable dconf-editor
-sudo pacman -S --noconfirm --needed dmidecode 
-sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
-sudo pacman -S --noconfirm --needed galculator geary gimp git gksu glances gnome-disk-utility 
-sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor 
-#sudo pacman -S --noconfirm --needed gnome-terminal
-sudo pacman -S --noconfirm --needed gparted gpick grsync
-sudo pacman -S --noconfirm --needed hardinfo hddtemp hexchat htop 
-sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors lsb-release meld mlocate mpv
-sudo pacman -S --noconfirm --needed nemo net-tools notify-osd numlockx openshot pinta plank polkit-gnome 
-sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell 
-sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat 
-sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk tumbler
-sudo pacman -S --noconfirm --needed variety vlc vnstat wget unclutter  
+sudo pacman -S --noconfirm --needed evolution
+sudo pacman -S --noconfirm --needed geary glances gnome-disk-utility 
+sudo pacman -S --noconfirm --needed gpick grsync
+sudo pacman -S --noconfirm --needed hddtemp  
+sudo pacman -S --noconfirm --needed inxi 
+sudo pacman -S --noconfirm --needed notify-osd pinta plank
+sudo pacman -S --noconfirm --needed redshift ristretto screenfetch shotwell 
+sudo pacman -S --noconfirm --needed simple-scan sysstat 
+sudo pacman -S --noconfirm --needed terminator transmission-cli
+sudo pacman -S --noconfirm --needed vnstat unclutter  
 
 
 sudo systemctl enable vnstat
@@ -35,7 +32,7 @@ sudo systemctl start vnstat
 ###############################################################################################
 
 # installation of zippers and unzippers
-sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils  uudeview  arj cabextract file-roller
+sudo pacman -S --noconfirm --needed unace sharutils  uudeview  arj cabextract 
 
 ###############################################################################################
 
